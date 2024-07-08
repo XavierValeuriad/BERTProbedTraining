b51faa8fc14474da42373f4cb4e618375db124eb
e187113j <xavier.pillet@ls2n.fr>
2024-07-08 11:36:56 +0200

    Changing number of GPUs.

--- preprocessing_dataset.sh
+++ preprocessing_dataset.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=48            # Nombre total de processus MPI
 #SBATCH --ntasks-per-node=8    # Nombre de processus MPI par noeud
 #SBATCH --hint=nomultithread   # 1 processus MPI par coeur physique (pas d'hyperthreading)
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=20
 #SBATCH --gpus-per-node=8
 #SBATCH --gres=gpu:8
 #SBATCH --partition=gpu_p2

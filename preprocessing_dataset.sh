fd29180188d4545f626f2de33b8e6f5f4c62264f
e187113j <xavier.pillet@ls2n.fr>
2024-07-08 16:40:56 +0200

    Tokenizing just bookcorpus dataset.

--- preprocessing_dataset.sh
+++ preprocessing_dataset.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=jeanzay
-#SBATCH --nodes=3
-#SBATCH --ntasks=24            # Nombre total de processus MPI
-#SBATCH --ntasks-per-node=8    # Nombre de processus MPI par noeud
-#SBATCH --hint=nomultithread   # 1 processus MPI par coeur physique (pas d'hyperthreading)
-##SBATCH --cpus-per-task=8
-#SBATCH --gpus-per-node=8
-#SBATCH --gres=gpu:8
-#SBATCH --partition=gpu_p2
-#SBATCH --qos=qos_gpu-t3
-#SBATCH --time=19:00:00
-#SBATCH --output=./logs/%x_%A_%a.out
-#SBATCH --error=./logs/%x_%A_%a.err
-#SBATCH -A mwd@v100
-
-module purge
 module load pytorch-gpu/py3/1.11.0
-
-nvidia-smi
 
 srun python preprocessing_dataset.py \
     --do_train \
@@ -26,7 +8,7 @@ srun python preprocessing_dataset.py \
     --log_level='info' \
     --logging_first_step='True' \
     --cache_dir='/cache/' \
-    --path_save_dataset="/data/tokenized_dataset" \
+    --path_save_dataset="/data/tokenized_bookcorpus" \
     --output_dir='/output' \
     --preprocessing_num_workers=20
 

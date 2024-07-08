f83a8d890c14cec95b675c6e5ae79bebbdbb952b
e187113j <xavier.pillet@ls2n.fr>
2024-07-08 22:20:03 +0200

    Tokenizing just small_bookcorpus dataset.

--- preprocessing_dataset.sh
+++ preprocessing_dataset.sh
@@ -3,14 +3,14 @@
 #module load pytorch-gpu/py3/1.11.0
 
 python3.9 preprocessing_dataset.py \
-    --train_file='data/bookcorpus.txt' \
+    --train_file='data/small_bookcorpus.txt' \
     --do_train \
     --overwrite_output_dir \
     --max_seq_length=512 \
     --log_level='info' \
     --logging_first_step='True' \
     --cache_dir='cache' \
-    --path_save_dataset='data/tokenized_bookcorpus' \
+    --path_save_dataset='data/tokenized_small_bookcorpus' \
     --output_dir='output' \
     --overwrite_cache='False' \
     --preprocessing_num_workers=20

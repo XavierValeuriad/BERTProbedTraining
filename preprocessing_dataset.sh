ea4a906f32de18a39b761bf138d65d6796ef7a2e
e187113j <xavier.pillet@ls2n.fr>
2024-07-09 01:54:29 +0200

    Tokenizing train_bert_25

--- preprocessing_dataset.sh
+++ preprocessing_dataset.sh
@@ -3,14 +3,14 @@ module purge
 module load pytorch-gpu/py3/1.11.0
 
 python3.9 preprocessing_dataset.py \
-    --train_file='data/train_bert_3.txt' \
+    --train_file='data/train_bert_25.txt' \
     --do_train \
     --overwrite_output_dir \
     --max_seq_length=512 \
     --log_level='info' \
     --logging_first_step='True' \
     --cache_dir='cache' \
-    --path_save_dataset='data/tokenized_train_bert_3' \
+    --path_save_dataset='data/tokenized_train_bert_25' \
     --output_dir='output' \
     --overwrite_cache='False' \
     --preprocessing_num_workers=20
--- textify_dataset.py
+++ textify_dataset.py
@@ -6,35 +6,17 @@ tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 from tqdm import tqdm
 from math import floor
 
-big_text = ''
-min_counter, max_counter, counter = 0, int(floor(0.33*len(raw_dataset))), 0
-print(f'len(raw_dataset) : {len(raw_dataset)}.')
-print(f'min_counter : {min_counter}.')
-print(f'max_counter : {max_counter}.')
-for example in tqdm(raw_dataset.select(range(min_counter, max_counter))):
-    big_text += f"{example['text']}\n"
+number_of_splits = 24
+percent_steps = 1.0/number_of_splits
 
-with open('train_bert_1.txt', 'w') as file:
-    file.write(big_text)
+for split_number in range(number_of_splits):
+    big_text = ''
+    min_counter, max_counter = int(floor(split_number*percent_steps*len(raw_dataset))), int(floor((split_number+1)*percent_steps*len(raw_dataset)))
+    print(f'len(raw_dataset) : {len(raw_dataset)}')
+    print(f'min_counter : {min_counter}')
+    print(f'max_counter : {max_counter}')
+    for example in tqdm(raw_dataset.select(range(min_counter, max_counter))):
+        big_text += f"{example['text']}\n"
+    with open(f'train_bert_{split_number+1}.txt', 'w') as file:
+        file.write(big_text)
 
-big_text = ''
-min_counter, max_counter, counter = int(floor(0.33*len(raw_dataset))), int(floor(0.67*len(raw_dataset))), 0
-print(f'len(raw_dataset) : {len(raw_dataset)}.')
-print(f'min_counter : {min_counter}.')
-print(f'max_counter : {max_counter}.')
-for example in tqdm(raw_dataset.select(range(min_counter, max_counter))):
-    big_text += f"{example['text']}\n"
-
-with open('train_bert_2.txt', 'w') as file:
-    file.write(big_text)
-
-big_text = ''
-min_counter, max_counter, counter = int(floor(0.67*len(raw_dataset))), len(raw_dataset), 0
-print(f'len(raw_dataset) : {len(raw_dataset)}.')
-print(f'min_counter : {min_counter}.')
-print(f'max_counter : {max_counter}.')
-for example in tqdm(raw_dataset.select(range(min_counter, max_counter))):
-    big_text += f"{example['text']}\n"
-
-with open('train_bert_3.txt', 'w') as file:
-    file.write(big_text)

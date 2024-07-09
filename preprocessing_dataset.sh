1346cd973c9a55e01dd9a9b4e916194900e10df3
e187113j <xavier.pillet@ls2n.fr>
2024-07-09 07:07:50 +0200

    Tokenizing train_bert1_3

--- preprocessing_dataset.sh
+++ preprocessing_dataset.sh
@@ -17,20 +17,7 @@ python3.9 preprocessing_dataset.py \
   --preprocessing_num_workers=20
 
 python3.9 preprocessing_dataset.py \
-  --train_file='data/train_bert_1.txt' \
-  --do_train \
-  --overwrite_output_dir \
-  --max_seq_length=512 \
-  --log_level='info' \
-  --logging_first_step='True' \
-  --cache_dir='cache' \
-  --path_save_dataset='data/tokenized_train_bert_1' \
-  --output_dir='output' \
-  --overwrite_cache='False' \
-  --preprocessing_num_workers=20
-
-python3.9 preprocessing_dataset.py \
-  --train_file='data/train_bert_1.txt' \
+  --train_file='data/train_bert_2.txt' \
   --do_train \
   --overwrite_output_dir \
   --max_seq_length=512 \
@@ -50,7 +37,7 @@ python3.9 preprocessing_dataset.py \
   --log_level='info' \
   --logging_first_step='True' \
   --cache_dir='cache' \
-  --path_save_dataset='data/tokenized_train_bert_1' \
+  --path_save_dataset='data/tokenized_train_bert_3' \
   --output_dir='output' \
   --overwrite_cache='False' \
   --preprocessing_num_workers=20

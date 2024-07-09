e4a113796cbab73b17a5444d88246646a019bb62
e187113j <xavier.pillet@ls2n.fr>
2024-07-09 07:03:52 +0200

    Tokenizing train_bert

--- preprocessing_dataset.sh
+++ preprocessing_dataset.sh
@@ -2,16 +2,21 @@
 module purge
 module load pytorch-gpu/py3/1.11.0
 
-python3.9 preprocessing_dataset.py \
-    --train_file='data/train_bert_25.txt' \
-    --do_train \
-    --overwrite_output_dir \
-    --max_seq_length=512 \
-    --log_level='info' \
-    --logging_first_step='True' \
-    --cache_dir='cache' \
-    --path_save_dataset='data/tokenized_train_bert_25' \
-    --output_dir='output' \
-    --overwrite_cache='False' \
-    --preprocessing_num_workers=20
+
+for i in $(seq 1 24);
+do
+    python3.9 preprocessing_dataset.py \
+      --train_file='data/train_bert_$i.txt' \
+      --do_train \
+      --overwrite_output_dir \
+      --max_seq_length=512 \
+      --log_level='info' \
+      --logging_first_step='True' \
+      --cache_dir='cache' \
+      --path_save_dataset='data/tokenized_train_bert_$i' \
+      --output_dir='output' \
+      --overwrite_cache='False' \
+      --preprocessing_num_workers=20
+done
+
 

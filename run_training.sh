c4ad7294e3f1df8ae2fe1b063ecc891b11b190d5
e187113j <xavier.pillet@ls2n.fr>
2024-07-09 11:27:28 +0200

    Test training.

--- run_training.sh
+++ run_training.sh
@@ -15,10 +15,10 @@
 #SBATCH --mail-type=begin,fail,abort,end
  
 # Nettoyage des modules charges en interactif et herites par defaut
-#module purge
-#
-## Chargement des modules
-#module load pytorch-gpu/py3/2.2.0
+module purge
+
+# Chargement des modules
+module load pytorch-gpu/py3/1.11.0
 #
 #python3.9 -m pip install --user --no-cache-dir -r requirements.txt
  

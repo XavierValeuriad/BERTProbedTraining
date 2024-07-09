fdcc7320ef273ed251999165192c023406f1e657
e187113j <xavier.pillet@ls2n.fr>
2024-07-09 07:43:04 +0200

    Tokenizing train_bert4_6

--- preprocessing_dataset.sh
+++ preprocessing_dataset.sh
@@ -4,40 +4,40 @@
 
 
 python3.9 preprocessing_dataset.py \
-  --train_file='data/train_bert_1.txt' \
+  --train_file='data/train_bert_4.txt' \
   --do_train \
   --overwrite_output_dir \
   --max_seq_length=512 \
   --log_level='info' \
   --logging_first_step='True' \
   --cache_dir='cache' \
-  --path_save_dataset='data/tokenized_train_bert_1' \
+  --path_save_dataset='data/tokenized_train_bert_4' \
   --output_dir='output' \
   --overwrite_cache='False' \
   --preprocessing_num_workers=20
 
 python3.9 preprocessing_dataset.py \
-  --train_file='data/train_bert_2.txt' \
+  --train_file='data/train_bert_5.txt' \
   --do_train \
   --overwrite_output_dir \
   --max_seq_length=512 \
   --log_level='info' \
   --logging_first_step='True' \
   --cache_dir='cache' \
-  --path_save_dataset='data/tokenized_train_bert_2' \
+  --path_save_dataset='data/tokenized_train_bert_5' \
   --output_dir='output' \
   --overwrite_cache='False' \
   --preprocessing_num_workers=20
 
 python3.9 preprocessing_dataset.py \
-  --train_file='data/train_bert_3.txt' \
+  --train_file='data/train_bert_6.txt' \
   --do_train \
   --overwrite_output_dir \
   --max_seq_length=512 \
   --log_level='info' \
   --logging_first_step='True' \
   --cache_dir='cache' \
-  --path_save_dataset='data/tokenized_train_bert_3' \
+  --path_save_dataset='data/tokenized_train_bert_6' \
   --output_dir='output' \
   --overwrite_cache='False' \
   --preprocessing_num_workers=20

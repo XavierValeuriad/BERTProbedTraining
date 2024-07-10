2df03ae28cd31a5ae839d812e46579ee67e0962f
e187113j <xavier.pillet@ls2n.fr>
2024-07-11 01:19:39 +0200

    Merge datasets.

--- merge_tokenized_datasets.sh
+++ merge_tokenized_datasets.sh
@@ -1,6 +1,7 @@
 module purge
-module load pytorch-gpu/py3/1.12.1
+module load pytorch-gpu/py3/2.2.0
 
 python3.9 -m pip install --user --no-cache-dir -r requirements_tokenization.txt
+
 
 python3.9 merge_tokenized_datasets.py

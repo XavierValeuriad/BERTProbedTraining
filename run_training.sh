e9e2cb0c8cca15702db5f37195bb291e69f6cd73
e187113j <xavier.pillet@ls2n.fr>
2024-07-10 23:53:30 +0200

    Ok.

--- run_training.sh
+++ run_training.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=mlm_bert_stats     # nom du job
 #SBATCH  -C a100
-#SBATCH --ntasks=2                   # nombre total de tache MPI (= nombre total de GPU)
-#SBATCH --gres=gpu:2                 # nombre de GPU par nœud (max 8 avec gpu_p2, gpu_p4, gpu_p5)
+#SBATCH --ntasks=128                   # nombre total de tache MPI (= nombre total de GPU)
+#SBATCH --gres=gpu:8                 # nombre de GPU par nœud (max 8 avec gpu_p2, gpu_p4, gpu_p5)
 #SBATCH --cpus-per-task=10           # nombre de CPU par tache (un quart du noeud ici)
 #SBATCH --hint=nomultithread         # hyperthreading desactive
-#SBATCH --time=0:20:00              # temps d'execution maximum demande (HH:MM:SS)
+#SBATCH --time=20:00:00              # temps d'execution maximum demande (HH:MM:SS)
 #SBATCH --output=./logs/mlm_test%j.out # nom du fichier de sortie
 #SBATCH --error=./logs/mlm_test%j.out  # nom du fichier d'erreur (ici commun avec la sortie)
 #SBATCH -A mwd@a100

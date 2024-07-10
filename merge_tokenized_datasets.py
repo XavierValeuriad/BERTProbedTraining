0649beb02719ffae58837549142532fee5da6eef
e187113j <xavier.pillet@ls2n.fr>
2024-07-11 01:14:09 +0200

    Merge datasets.

--- merge_tokenized_datasets.py
+++ merge_tokenized_datasets.py
@@ -1,49 +1,11 @@
-import logging, os, sys
+import logging, sys
 
 from datasets import load_from_disk, concatenate_datasets
 from datasets.utils.logging import set_verbosity
 
-from transformers import (
-    MODEL_FOR_MASKED_LM_MAPPING,
-    HfArgumentParser,
-    TrainingArguments,
-    set_seed,
-)
-from transformers.trainer_utils import get_last_checkpoint
-from transformers.utils.versions import require_version
-
-from transformers.utils.logging import set_verbosity, enable_default_handler, enable_explicit_format
-from transformers.utils.logging import set_verbosity as transformer_set_verbosity
-from torch.distributed.elastic.multiprocessing.errors import record
-
-require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/language-modeling/requirements_training.txt")
-
 logger = logging.getLogger(__name__)
-MODEL_CONFIG_CLASSES = list(MODEL_FOR_MASKED_LM_MAPPING.keys())
-MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
 
-@record
 def main():
-
-    # See all possible arguments in src/transformers/training_args.py
-    # or by passing the --help flag to this script.
-    # We now keep distinct sets of args, for a cleaner separation of concerns.
-
-    # os.environ['LOCAL_RANK'] = os.environ['SLURM_LOCALID']  # COMM
-
-    # COMM
-    # dist.init_process_group(backend='nccl',
-    #         init_method='env://',
-    #         world_size=idr_torch.size,
-    #         rank=idr_torch.rank)
-
-    parser = HfArgumentParser((TrainingArguments, ))
-    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
-        # If we pass only one argument to the script and it's the path to a json file,
-        # let's parse it to get our arguments.
-        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
-    else:
-        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
     # Sending telemetry. Tracking the example usage helps us better allocate resources to maintain them. The
     # information sent is the one passed as arguments along with your Python/PyTorch versions.
@@ -61,53 +23,12 @@ def main():
     log_level = logging.DEBUG
     logger.setLevel(log_level)
     set_verbosity(log_level)
-    transformer_set_verbosity(log_level)
-    enable_default_handler()
-    enable_explicit_format()
 
     # print(training_args.device)
 
     # Log on each process the small summary:
-    logger.warning(
-        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
-        + f" distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
-    )
-    # Set the verbosity to info of the Transformers logger (on main process only):
-    logger.info(f"Training/evaluation parameters {training_args}.")
 
-    # Detecting last checkpoint.
-    last_checkpoint = None
-    if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
-        last_checkpoint = get_last_checkpoint(training_args.output_dir)
-        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
-            raise ValueError(
-                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
-                "Use --overwrite_output_dir to overcome."
-            )
-        elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
-            logger.info(
-                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
-                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
-            )
 
-    # Set seed before initializing model.
-    set_seed(training_args.seed)
-
-    # Get the datasets: you can either provide your own CSV/JSON/TXT training and evaluation files (see below)
-    # or just provide the name of one of the public datasets available on the hub at https://huggingface.co/datasets/
-    # (the dataset will be downloaded automatically from the datasets Hub
-    #
-    # For CSV/JSON files, this script will use the column called 'text' or the first column. You can easily tweak this
-    # behavior (see below)
-    #
-    # In distributed training, the load_dataset function guarantee that only one local process can concurrently
-    # download the dataset.
-
-    # bookcorpus = load_dataset("bookcorpus", cache_dir=model_args.cache_dir, use_auth_token=True if model_args.use_auth_token else None)
-    # wiki = load_dataset("wikipedia", "20220301.en", cache_dir=model_args.cache_dir, use_auth_token=True if model_args.use_auth_token else None)
-    # wiki = wiki.remove_columns([col for col in wiki.column_names if col != "text"])  # only keep the 'text' column
-    #
-    # assert bookcorpus.features.type == wiki.features.type
     print('Starting to merge the datasets.')
 
     full_dataset = load_from_disk('data/tokenized_train_bert_1')
@@ -120,7 +41,7 @@ def main():
         full_dataset['validation'] = concatenate_datasets(
             [full_dataset['validation'], loaded_dataset['validation']]
         )
-        full_dataset.save_to_disk(data_args.path_save_dataset)
+        full_dataset.save_to_disk(f'data/tokenized_train_bert_{i}/24_complete')
 
 
     print("End script")
--- merge_tokenized_datasets.sh
+++ merge_tokenized_datasets.sh
@@ -3,14 +3,4 @@ module load pytorch-gpu/py3/1.11.0
 
 python3.9 -m pip install --user --no-cache-dir -r requirements_tokenization.txt
 
-python3.9 merge_tokenized_datasets.py \
-  --do_train \
-  --overwrite_output_dir \
-  --max_seq_length=512 \
-  --log_level='info' \
-  --logging_first_step='True' \
-  --cache_dir='cache' \
-  --path_save_dataset='data/tokenized_train_bert_full' \
-  --output_dir='output' \
-  --overwrite_cache='False' \
-  --preprocessing_num_workers=20
+python3.9 merge_tokenized_datasets.py

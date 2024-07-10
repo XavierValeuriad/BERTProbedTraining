a55a0d7291d7a2448cebf347e02b924fc2040450
e187113j <xavier.pillet@ls2n.fr>
2024-07-10 17:50:14 +0200

    Fixing args.

--- run_training.sh
+++ run_training.sh
@@ -18,7 +18,7 @@
 module purge
 
 # Chargement des modules
-module load pytorch-gpu/py3/1.11.0
+module load pytorch-gpu/py3/2.3.0
 #
 #python3.9 -m pip install --user --no-cache-dir -r requirements.txt
  

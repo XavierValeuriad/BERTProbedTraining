bae3b474066bd28098168b2ba7a9ac08c600fce9
e187113j <xavier.pillet@ls2n.fr>
2024-07-10 23:34:03 +0200

    Local test of the training hooks for statistics.

--- test_run_training.py
+++ test_run_training.py
@@ -814,18 +814,18 @@ def main():
 
     # callback = CallbackForGradientStatistics()
 
-    metric = evaluate.load("./accuracy.py")
-
-    def compute_metrics(eval_preds):
-        preds, labels = eval_preds
-        # preds have the same shape as the labels, after the argmax(-1) has been calculated
-        # by preprocess_logits_for_metrics
-        labels = labels.reshape(-1)
-        preds = preds.reshape(-1)
-        mask = labels != -100
-        labels = labels[mask]
-        preds = preds[mask]
-        return metric.compute(predictions=preds, references=labels)
+    # metric = evaluate.load("./accuracy.py")
+    #
+    # def compute_metrics(eval_preds):
+    #     preds, labels = eval_preds
+    #     # preds have the same shape as the labels, after the argmax(-1) has been calculated
+    #     # by preprocess_logits_for_metrics
+    #     labels = labels.reshape(-1)
+    #     preds = preds.reshape(-1)
+    #     mask = labels != -100
+    #     labels = labels[mask]
+    #     preds = preds[mask]
+    #     return metric.compute(predictions=preds, references=labels)
 
     # Initialize our Trainer
     trainer = Trainer(
